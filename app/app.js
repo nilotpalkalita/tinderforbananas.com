3f5896e82296183116a9aab01222e93e350d6518
Nilotpal Kalita <nilotpalkalita@hotmail.com>
2017-01-24 14:35:10 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -4,7 +4,7 @@
       id: 0,
       name: 'Saba',
       age: 25,
-      job: 'Phillippines',
+      job: 'Shake-ing',
       images: ['images/testpic4.jpg', 'images/testpic2.jpg', 'images/testpic1.jpg'],
       distance: 25,
       description: 'Swipe for me! You’ll find me very ap-peel-ing'
@@ -13,7 +13,7 @@
       id: 1,
       name: 'Plantain',
       age: 28,
-      job: 'Nicaragua',
+      job: 'Eat-ing',
       images: ['images/testpic2.jpg', 'images/testpic4.jpg'],
       distance: 4,
       description: 'Lorem ipsum dolor sit amet, quo ad cibo viris legimus, simul delicata constituto per cu. Pro an commodo liberavisse, cu mutat sensibus tractatos est, animal similique ei nec. Et est molestie phaedrum, ut eam quot meliore. Usu hendrerit complectitur at, at iriure habemus facilisis sit. An eos probo graece.Propriae contentiones eu ius, pro eu ignota liberavisse disputationi, duo ea docendi consectetuer. Cum posse semper ea, ius invidunt qualisque scriptorem cu, ullum reprehendunt pro eu. Illud erant reformidans usu in. Ad vim quem choro iracundia. Ius in case mnesarchum.Duis signiferumque sed cu. Ut duo error congue intellegebat, fugit nostrud urbanitas ei has. Copiosae dissentias te eam, dicta efficiendi mea ad. Numquam persequeris te sea, ad populo graeci per, et mea aperiam noluisse interesset.Malorum abhorreant pri eu, no vidit quaeque mei, usu in dico meliore philosophia. Causae verterem pri in, te case suavitate nam. In ius ignota sanctus. Propriae repudiandae ad sit, gubergren ullamcorper usu ei. Ne vis fierent mediocritatem. Id nominati maluisset ius, soluta graece lobortis ut his, vocibus copiosae placerat est ad.Duo alia ferri impetus ei, deleniti scriptorem comprehensam ius an. Mea ne labore oblique adolescens. Ne velit albucius salutatus quo, cum iudico eripuit bonorum ad. Stet suscipit sea ad. Nec prompta suscipit mandamus at.'
